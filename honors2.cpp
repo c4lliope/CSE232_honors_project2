a29b1f4c517800fa5271098204ecb73c7e028242
Michael Wright <wrigh517@gmail.com>
2014-04-25 13:06:33 -0400

    getting segfault on g.display

--- honors2.cpp
+++ honors2.cpp
@@ -25,10 +25,6 @@ using std::map;
 using std::ostream;
 using std::pair;
 
-struct Player{
-  public:
-    vector<(Space*)> player_spaces_;
-};//of Player
 
 struct Die{
   //default_random_engine dre;
@@ -53,7 +49,7 @@ struct Space{
     Space *down_;
     Space *left_;
     Space *right_;
-    
+
     void add_die(Die die){
       if(dice_.size() < 7)
         dice_.push_back(die);
@@ -81,42 +77,34 @@ struct Space{
 
 };//of space      
 
-struct Grid{
+struct Player{
   public:
-    int h_rows_;
-    int v_columns_;
-    long total_spaces_;
-    
-    map< string, Space* > grid_coords_;
+    vector< Space* > player_spaces_;
+};//of Player
 
-    ostream& display(ostream& out){
-      out << "Trying to print" << endl;
-      for (int h = h_rows_-1; h > -1; h--){
-        for (int v = 0; v < v_columns_; v++){
-          string h_str = to_string(h);
-          string v_str = to_string(v);
-          string coords = h_str+':'+v_str;
-          auto map_itr = (grid_coords_.find(coords));
-          out << map_itr->second->h_index_ << ':' << map_itr->second->v_index_ << ' ';
-        }
-        out << endl;
-      }
-      return out;
-    }//of display
+struct Grid{
+  private:
+    vector< vector< Space* > > grid_;
+    int const h_rows_ = 6;
+    int const v_columns_ = 6;
+    long const total_spaces_ = 36;
+  public:
 
-    Grid(int h_rows, int v_columns){
-      h_rows_ = h_rows; v_columns_ = v_columns;
-      total_spaces_ = (h_rows * v_columns);
-      cout << "Making Grid: " << h_rows << ':' << v_columns << endl;
-      for (int h = 0; h < h_rows_; h++){
-        for (int v = 0; v < v_columns_; v++){
-          string h_str = to_string(h);
-          string v_str = to_string(v);
-          string coords = h_str+':'+v_str;
-          Space *set_space = (new Space(h, v));
-          grid_coords_[coords] = set_space;
 
+    Grid(){
+      for (int h = 0; h < h_rows_; h++) {
+        // add a new row
+        vector< Space* > row_h;
+        row_h.clear();
+        grid_.push_back(row_h);
+
+        for (int v = 0; v < v_columns_; v++) {
+          Space *set_space = new Space(h, v); // these arguments might be backwards
+          row_h.push_back(set_space);
+
+          //middle cases
           if ((h != 0) && (h != 5) && (v != 5) && (v != 0)){
+            cout << "Middle case" << endl;
             if (set_space->up_ == nullptr)
               set_space->up_ = new Space(h,v+1);
             if (set_space->down_ == nullptr)
@@ -129,6 +117,7 @@ struct Grid{
 
           //non-middle cases
           else{
+            cout << "Corner Case "<< endl;
             if (h == 0 && v == 0){
               set_space->left_ == nullptr;
               set_space->down_ == nullptr;}
@@ -143,6 +132,7 @@ struct Grid{
               set_space->down_ == nullptr;}
 
             //edge-only cases
+            cout << "Edge Case "<< endl;
             if (h == 5) //top-edge
               set_space->up_ == nullptr;
             if (h == 0) // bottom-edge
@@ -152,45 +142,47 @@ struct Grid{
             if (v == 0) //left-side
               set_space->left_ == nullptr;
           }//of else
-        
         }
       }
-    }//of Grid
-};//of Grid
-//used to set spaces and their pointers and owners at begining of match
+      cout << endl << endl << "Finished construction" << endl;
+    }//of Grid constructor
 
-class Game{
-  private:
-    map<string, Space*> coords_left_;
-    //the colomns and rows for space coordinates (0-5) ... think battleship
-  public:
-    Game(Grid &grid, default_random_engine &dre,Player &p1, Player &p2){
-      coords_left_ = grid.grid_coords_;
-      while(coords_left_.size() > (grid.total_spaces_/2))
-      {
-        auto it = coords_left_.begin();
-        advance(it, rand() % coords_left_.size());
-        Space* random_space = it->second;
-        p1.player_spaces_.push_back(random_space); 
-        coords_left_.erase(it->first);
-      }//of for
-      cout << endl;
-      for (auto elem:coords_left_)
-        cout << elem.first<< endl;
-    }//of constructor
-};//of GameBoard
+    //ostream& display(ostream& out){
+    void display(){
+      //out << "Printing contents of row: ";
+      cout << "Printing contents of row: ";
+      for (auto elem : grid_) {
+        //out << "Printing contents of row: ";
+        cout << "Printing contents of row: ";
+        for (int v = 0; v < v_columns_; v++) {
+          Space *print_space;
+          print_space = (elem[v]); 
+          cout << print_space->v_index_ << ", " << print_space->h_index_ << endl;
+          //out << print_space->v_index_ << ", " << print_space->h_index_ << endl;
+        }
+      }
+      //return out;
+    }//of display
+
+};//of Grid
+
+//used to set spaces and their pointers and owners at begining of match
 
 
 int main(){
   long seed = 998765;
   default_random_engine dre(seed);
-  Grid g(6,6);
-  g.display(cout);
+  cout << "Making grid in main...." << endl;
+  Grid g; 
+  cout << "Printing grid in main...." << endl;
+  g.display();
   Die d; 
 
   cout << endl << "Rolling dice:" << endl;
   for (auto i = 0;i < 20; i++)
     d.roll(dre);
   return 0;
+
+
 }//of main
 
